b0ddfd3edd3903550638313bd51d4bc080ec115b
abdellah96 <abdellah.elazzam@gmail.com>
2017-03-04 16:48:30 +0100

    Add files via upload

--- exSDL.c
+++ exSDL.c
@@ -1,4 +1,4 @@
-#include "grid_color.h"
+#include "struct.h"
 #include <SDL/SDL.h>
 #include <SDL/SDL_ttf.h>
 
@@ -74,36 +74,48 @@ void printcouleurs(SDL_Surface *ecran){
 }
 
 void jouer(SDL_Surface *ecran,grid g,char c){
-	detect_flood(&g,0,0,g.array[0][0]);
-	change_color(&g,c);
+	identiftache(g,0,0,g.array[0][0]);
+	changecolor(g,c);
 	printgrille(ecran,g);
-	refresh_grid(&g);
+	refreshgrille(g);
 }
 
 
 
 
-int main() {
-	grid g;
-	int size;
+int main(int argc, char *argv[]) {
+	grid g;int size,nbcoups;
 	printf("Entrer la taille du grille ");
 	scanf("%d",&size);
+	printf("Entrer le nombre de coups ");
+	scanf("%d",&nbcoups);
+	nbcoups=nbcoups;
+	SDL_Rect position;
 	g=init_grid(size);
 	int continuer=1;
-	SDL_Surface *ecran = NULL;
+	SDL_Surface *ecran = NULL,*texte=NULL;
 	SDL_Event event;
   const SDL_VideoInfo* info = NULL;
   SDL_Surface *ima=NULL;
+
 
   if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
       /* Failed, exit. */
       fprintf( stderr, "Video initialization failed: %s\n", SDL_GetError( ) );
       SDL_Quit( );
   }
+
 	if(TTF_Init() == -1){
-		fprintf(stderr, "Erreur d'initialisation de TTF_Init : %s\n", TTF_GetError());
-    	exit(EXIT_FAILURE);
+    fprintf(stderr, "Erreur d'initialisation de TTF_Init : %s\n", TTF_GetError());
+    exit(EXIT_FAILURE);
 	}
+	TTF_Font *police = NULL;
+	police = TTF_OpenFont("angelina.ttf", 65);
+	SDL_Color couleurNoire = {0, 0, 0},couleurBlanche = {255, 255, 255};
+	int nbr_mvm=0;
+	char mouvement[100];
+	position.x = 300;
+	position.y = 750;
 
 
 	info = SDL_GetVideoInfo( );
@@ -113,7 +125,7 @@ int main() {
       SDL_Quit( );
   }
 
-	ecran=SDL_SetVideoMode(600,700, 32, SDL_HWSURFACE|SDL_FULLSCREEN);
+	ecran=SDL_SetVideoMode(600,800, 32, SDL_HWSURFACE);
 	SDL_WM_SetCaption("ColorFlood", NULL);
 
 
@@ -123,44 +135,63 @@ int main() {
 	grid_print(&g);
 	printgrille(ecran,g);
 	printcouleurs(ecran);
-	SDL_PauseAudio(0);
-	SDL_Delay(100);
 
-	int nbr_mvm=0;
-  while (continuer && !test_same_colour(&g)) {
+	sprintf(mouvement,"%d",nbcoups);
+	texte = TTF_RenderText_Shaded(police, mouvement,couleurNoire,couleurBlanche);
+	SDL_BlitSurface(texte, NULL, ecran,&position);
+
+
+
+  while (continuer && !test_same_colour(&g) && nbcoups>0) {
 		SDL_WaitEvent(&event);
     switch(event.type) {
         case SDL_QUIT:
             continuer = 0;
             break;
+				case SDL_KEYDOWN:
+            switch (event.key.keysym.sym)
+            {
+                case SDLK_ESCAPE: /* Appui sur la touche Echap, on arrête le programme */
+                    continuer = 0;
+                    break;
+                default:break;
+            }
 				case SDL_MOUSEBUTTONUP:
 						if (event.button.button == SDL_BUTTON_LEFT){
-							if(event.button.y>610){
+							if(event.button.y>610 && event.button.y<700){
 								nbr_mvm++;
-								if(event.button.x<100){
-									jouer(ecran,g,'V'); break;
-								}
-								if(event.button.x<200){
-									jouer(ecran,g,'R'); break;
-								}
-								if(event.button.x<300){
-									jouer(ecran,g,'B'); break;
-								}
-								if(event.button.x<400){
-									jouer(ecran,g,'J'); break;
-								}
-								if(event.button.x<500){
-									jouer(ecran,g,'M'); break;
-								}
-								if(event.button.x<600){
-									jouer(ecran,g,'G'); break;
-								}
-							}
-						}
-			}
+								drawRectangle(ecran,300,750,100,255,255,255);
+								nbcoups--;
+								sprintf(mouvement,"%d",nbcoups);
+								texte = TTF_RenderText_Shaded(police, mouvement,couleurNoire,couleurBlanche);
+								SDL_BlitSurface(texte, NULL, ecran,&position);
 
-  }
-	printf("Vous avez resolu la grille en %d mouvements \n",nbr_mvm);
+									if(event.button.x<100){
+										jouer(ecran,g,'V'); break;
+									}
+									if(event.button.x<200){
+										jouer(ecran,g,'R'); break;
+									}
+									if(event.button.x<300){
+										jouer(ecran,g,'B'); break;
+									}
+									if(event.button.x<400){
+										jouer(ecran,g,'J'); break;
+									}
+									if(event.button.x<500){
+										jouer(ecran,g,'M'); break;
+									}
+									if(event.button.x<600){
+										jouer(ecran,g,'G'); break;
+									}
+								}
+						}
+					}
+			}
+	if(test_same_colour(&g)) printf("Vous avez resolu la grille en %d mouvements \n",nbr_mvm);
+	else printf("Vous avez perdu");
+	TTF_CloseFont(police);
+	TTF_Quit();
 	SDL_Quit();
 
 	return 0;
