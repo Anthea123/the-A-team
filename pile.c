292aad1168fb13b85c8f0fc870b5b26f740693d5
Anthea Mérida <anthea.merida@gmail.com>
2017-03-24 12:41:16 +0100

    ajout de free_pile

--- pile.c
+++ pile.c
@@ -3,25 +3,41 @@
 #include <stdbool.h>
 
 struct pile {
-    char val;
-  	pile* next;
+	char val;
+	pile* next;
 };
+
 pile* init_pile(void){
-    return 0;
+	return 0;
 }
+
 bool est_vide(pile* p) {
-  return (0 == p);
+	return (0 == p);
 }
 
 void push(pile** pp,char c) {
-  pile* r = malloc(sizeof *r);
-  r->val=c;
-  r->next=*pp;
-  *pp = r;
+	pile* r = malloc(sizeof *r);
+	r->val=c;
+	r->next=*pp;
+	*pp = r;
 }
 
 void pop(pile** pp) {
-  pile* tmp=(*pp)->next;
-  free(*pp);
-  *pp = tmp;
+	pile* tmp=(*pp)->next;
+	free(*pp);
+	*pp = tmp;
+}
+
+void free_pile(pile **pp){
+	while(!est_vide(*pp)){
+		pop(pp);
+	}
+}
+
+char get_head(pile *p){
+	return p->val;
+}
+
+pile* get_next(pile *p){
+	return p->next;
 }
--- pile.h
+++ pile.h
@@ -1,7 +1,17 @@
 #include <stdbool.h>
 
 typedef struct pile pile;
+
 pile* pile_init(void);
+
 bool est_vide(pile*);
+
 void push(pile**, char);
+
 void pop(pile**);
+
+char get_head(pile *p);
+
+pile* get_next(pile *p);
+
+void free_pile(pile **pp);
