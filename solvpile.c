3c80afc3aad56c14d1564be092f8b60c12b02de9
HenL <loubna.hennach@hotmail.fr>
2017-04-02 15:38:02 +0200

    Update solvpile.c

--- solvpile.c
+++ solvpile.c
@@ -9,86 +9,102 @@
 #include "pile.h"
 #include "solvpile.h"
 
-struct solvpile {
+struct solvpile 
+{
 	pile *sol;
 	solvpile *next;
-	};
+};
 
-solvpile* init_solvpile(void){
-		return NULL;
+solvpile* init_solvpile(void)
+{
+	return NULL;
 }
-bool solvest_vide(solvpile* p) {
+
+bool solvest_vide(solvpile* p) 
+{
 	return (0 == p);
 }
 
-void solvpush(solvpile** pp, pile* v) {
+void solvpush(solvpile** pp, pile* v) 
+{
 
-	if(!est_vide(v)){
+	if(!est_vide(v))
+	{
 		solvpile* r = malloc(sizeof *r);
 		r->sol=v;
 		r->next=*pp;
 		*pp = r;
 	}
-	else{
+	else
+	{
 		printf("v est vide");
 	}
 }
 
-pile* minpile(solvpile* p){
-	if(solvest_vide(p)){
-		return NULL;
-	}
-	else{
-		pile *min=p->sol;
-		int l=pilelen(min);
-		while(p!=NULL){
-			if(l>pilelen(p->sol)){
-				l=pilelen(p->sol);
-				min=p->sol;
-			}
-			p=p->next;
+pile* minpile(solvpile* p)
+{
+	pile *min=p->sol;
+	int l=pilelen(min);
+	while(p!=NULL)
+	{
+		if(l>pilelen(p->sol))
+		{
+			l=pilelen(p->sol);
+			min=p->sol;
 		}
-		return min;
+		p=p->next;
 	}
+return min;
 }
 
 
-void solvpop(solvpile** pp) {
+void solvpop(solvpile** pp) 
+{
 	solvpile* tmp=(*pp)->next;
 	free(*pp);
 	*pp = tmp;
 }
 
-void print_solvpile(solvpile *p){
+void print_solvpile(solvpile *p)
+{
 	if(p==NULL) printf("Pile vide");
-	while(p!=NULL){
+	while(p!=NULL)
+	{
 		print_pile(p->sol);
 		printf("\n");
 		p=p->next;
 	}
 }
 
-void free_solvpile(solvpile **pp){
-	while(!solvest_vide(*pp)){
+void free_solvpile(solvpile **pp)
+{
+	while(!solvest_vide(*pp))
+	{
 		solvpop(pp);
 	}
 }
 
 
-pile * solvget_head(solvpile *pp){
-	if(!solvest_vide(pp)){
+pile * solvget_head(solvpile *pp)
+{
+	if(!solvest_vide(pp))
+	{
 		return pp->sol;
 	}
-	else{
+	else
+	{
 		return NULL;
 	}
 }
 
-solvpile * solvget_next(solvpile *pp){
-	if(!solvest_vide(pp)){
-	return pp->next;
+solvpile * solvget_next(solvpile *pp)
+{
+	if(!solvest_vide(pp))
+	{
+		return pp->next;
 	}
-	else{
+	else
+	{
 		return NULL;
 	}
 }
