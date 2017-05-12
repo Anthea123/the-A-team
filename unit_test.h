3fc4492d215931048d6f2a94de43531e44d6c253
HenL <loubna.hennach@hotmail.fr>
2017-05-12 02:25:26 +0200

    Update unit_test.h

--- unit_test.h
+++ unit_test.h
@@ -3,25 +3,16 @@
 
 /**
 *	\file unit_test.h
-*	\brief prototypes des fonctions et structures nécessaires pour les tests unitaires des fonctions contenues dans pile.c et solveur.c
+*	\brief prototypes des fonctions et structures nécessaires pour les tests unitaires des fonctions du solveur_rapide
 *	\author Loubna Anthea Abdellah Bader
-*	\version 1.0
-*	\date 24 Mar 2017
+*	\version 3.0
+*	\date 10 Mai 2017
 */
 
 #include <stdbool.h>
 #include "CUnit/CUnit.h"
 #include "CUnit/Basic.h"
 #include "grid_color.h"
-#include "grid_tree.h"
-#include "solveur.h"
 
-/***************** tests pour les NArbres *************************/
-
-bool test_egalite(grid g1, grid g2);
-
-void test_nouvelNArbre(void);
-
-void test_ajoutFils(void);
 
 #endif
